1aac485dfa5e63a6ab81093ed99a753b31ec3d56
m3ia <meianatividad@gmail.com>
2022-08-17 07:45:33 -0700

    Add parentGrid logic

--- script.js
+++ script.js
@@ -61,3 +61,91 @@ matrix.forEach((elem, i, arr) => {
 let tr = doc.createElement('tr');
 let td = doc.createElement('td');
 
+// Get the array of everything in the parent of row 2, column 2 (9) 
+// should be [1, 2, 8, 9, 7, 3, 5, 4, 6]
+// Going to create a grid with the index of each item in the string:
+
+let indexMatrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+
+let sampleStr = "";
+for (let i = 0; i < 81; i++) {
+    sampleStr+=(i % 9);
+}
+rowNum = 0; colNum = 0;
+for (let i = 0; i < sampleStr.length; i++) {
+    indexMatrix[rowNum][colNum] = i;
+    if (rowNum === 8 && colNum === 8) {
+        break;
+    } 
+    // Create index for next loop
+    colNum++;
+    if (colNum > 8) {
+        colNum = 0;
+        rowNum++;
+    }
+    if (rowNum > 8) {
+    }
+}
+
+console.log(matrix);
+
+// Get indexMatrix grid
+let indexMatrixGrid = doc.getElementsByClassName('indexMatrixGrid')[0];
+
+indexMatrix.forEach((elem, i, arr) => {
+    let tr = doc.createElement('tr');
+    indexMatrixGrid.appendChild(tr);
+    elem.forEach((elem) => {
+        let td = doc.createElement('td');
+        tr.appendChild(td);
+        td.innerHTML = elem;
+    })
+})
+
+let tr2 = doc.createElement('tr');
+let td2 = doc.createElement('td');
+
+//  create an object for each item? as we loop through the string, we'll push the number to its "parent" array.
+// every 3 iterations, parent goes up by 1 until it hits 9
+// any time the parent array hits a length of 9, update parent count to 
+
+// let sampleStr = "";
+// for (let i = 0; i < 81; i++) {
+//     sampleStr+=(i % 9);
+// }
+console.log('sampleStr', sampleStr);
+
+let parentGrid = [ [], [], [], [], [], [], [], [], []];
+
+// Algorithm for creating Parent Gride data structure
+//  we want: parentGrid[0] = [0, 1, 2, 0, 1, 2, 0, 1, 2];
+for (let i = 0; i < sampleStr.length; i++) { // 75 @ 3,8 @ 1, 2
+    // Gets the exact row and column for the item (x is between 0-8, y is between 0-8)
+    let column = i % 9; // 3
+    let row = Math.floor(i / 9); // 8
+    // Gets the exact coordinates 
+    let parentGridCol = Math.floor(column / 3); // 1
+    let parentGridRow = Math.floor(row / 3); // 2
+    // Push to respective parent array
+    let parentArr = (parentGridRow * 3) + (parentGridCol); // 7
+    parentGrid[parentArr].push(sampleStr[i]); // parentGrid[7].includes 75 
+}
+console.log(parentGrid[7].includes(75)); // true
+console.log(parentGrid[0]); //  we want: parentGrid[0] = [0, 1, 2, 0, 1, 2, 0, 1, 2];
+// [0, 1, 2, 9, 10, 11, 18, 19, 20]
+// i created a grid of all the indices and looked for a mathematical way to push each item to a data structure. ended up with a triple-nested loop
+// created large coordinates to represent parents, then sought a mathematical way to see where an index would fall into. on the way, i figured out a way to identify the exact row and column of an index: to get the column, use % 9 since the entire first column is a multiple of 9. to get the row, do Math.floor(index/9) since i know every 27 numbers will fall between a parent row
+// the next problem was figuring out how to map these exact coordinates to the larger parent grid where the min is 0 and the max is 2 on both the y and x axes. all i wanted was to push the respective item to a parentGrid array where the index matches up with the parent. Then I asked for help lol: let parentArr = (parentGridRow * 3) + (parentGridCol)./
+// Apparently this line allows us to flatten the parent grid out, and tells us which parent box the item lives in.
+// parentGridRow - mult by 3 bc if we flatten the grid, it'd be in box index 3. 
